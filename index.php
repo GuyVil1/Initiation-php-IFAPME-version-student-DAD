24e0f1d546be0f2a19e98cb0447f180b0b044385
GuyVil1 <guy.vilain1@gmail.com>
2021-04-12 19:26:52 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>introduction</title>
+</head>
+<body>
+<p>
+    <button><a href="./exemple/exemple001.php">Exemple001</a></button>
+    <button><a href="./exemple/exemple002.php">Exemple002</a></button>
+    <button><a href="./exemple/exemple003.php">Exemple003</a></button>
+    <button><a href="./exemple/exemple004.php">Exemple004</a></button>
+</p>
+
+<p>
+<button><a href="./exercices/exercice001.php">Exercice001</a></button>
+<button><a href="./exercices/exercice002.php">Exercice002</a></button>
+<button><a href="./exercices/exercice003.php">Exercice003</a></button>
+
+</p>
+ 
+</body>
+
+</html>
